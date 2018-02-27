4411bdf9e34242e0a12f92c1d1aba041b15534f4
Chong <aaa59891@gmail.com>
2018-02-27 14:56:56 +0800

    Communiacte in defferent component

--- src/components/Server.vue
+++ src/components/Server.vue
@@ -1,11 +1,35 @@
 <template>
-    <li class="list-group-item">
+    <li class="list-group-item" :class="{error: status === 'Error'}" @click="showStatus">
         Server #{{ index }}
     </li>
 </template>
 
 <script>
+import { eventBus } from '../main.js';
     export default {
-        props:['index']
+        data: function(){
+            return {
+                status: "Normal"
+            };
+        },
+        props:['index'],
+        methods:{
+            showStatus(){
+                eventBus.$emit('showServerInfo', {index: this.index, status: this.status});
+            }
+        },
+        created(){
+            eventBus.$on('changeStatus', (index, status) => {
+                if(index !== this.index){
+                    return;
+                }
+                this.status = status;
+            })
+        }
     };
 </script>
+<style scoped>
+    .error{
+        border: 1px solid red;
+    }
+</style>
--- src/components/ServerInfo.vue
+++ src/components/ServerInfo.vue
@@ -1,5 +1,36 @@
 <template>
     <div class="col-xs-12 col-sm-6">
-        <p>Server Details are currently not updated</p>
+        <div v-if="show">
+            <p>Server status: {{ status }}</p>
+            <button @click="changeStatus">Change Status</button>
+        </div>
+        <p v-else>Server Details are currently not updated</p>
     </div>
 </template>
+<script>
+    import {
+        eventBus
+    } from '../main.js';
+    export default {
+        data: function () {
+            return {
+                show: false,
+                index: 0,
+                status: ''
+            }
+        },
+        methods: {
+            changeStatus(){
+                this.status = "Error";
+                eventBus.$emit('changeStatus', this.index, this.status);
+            }
+        },
+        created() {
+            eventBus.$on('showServerInfo', (serverInfo) => {
+                this.show = true;
+                this.index = serverInfo.index;
+                this.status = serverInfo.status;
+            })
+        }
+    }
+</script>
